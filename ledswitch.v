27e539ed986f6aa6eb6c1bf6981f7c7ed09375d9
Azatrum2005 <mrangwala15@gmail.com>
2026-05-01 18:44:16 +0400

    Update ledswitch.v

--- ledswitch.v
+++ ledswitch.v
@@ -1,13 +1,8 @@
 module ledswitch(
-    // Input: Slide Switch 0
     input wire [0:0] SW,
-    // Output: Arduino Header Pin 0 (Digital Pin 0)
     inout wire [0:0] ARDUINO_IO,
 	 inout wire [0:0] GPIO // Changed from ARDUINO_IO
 );
-    // ==========================================
-    // LOGIC: Direct Connection
-    // ==========================================
     // When SW[0] is UP (1), ARDUINO_IO[0] outputs 3.3V (HIGH)
     // When SW[0] is DOWN (0), ARDUINO_IO[0] outputs 0V (LOW)
     assign ARDUINO_IO[0] = SW[0];
